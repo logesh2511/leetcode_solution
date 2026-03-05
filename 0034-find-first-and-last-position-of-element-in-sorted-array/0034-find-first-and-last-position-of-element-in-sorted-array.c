bf2d0a4032c0965c65ab9554ce4386c7ec11185c
Logesh <159474479+logesh2511@users.noreply.github.com>
2026-03-06 01:53:08 +0530

    Time: 8 ms (0.47%), Space: 7.3 MB (100%) - LeetHub

---
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    result[0] = -1;
+    result[1] = -1;
+    *returnSize = 2;
+
+
+   
+    int left = 0;
+    int right = numsSize - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            result[0] = mid;
+            right = mid - 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+
+   
+    left = 0;
+    right = numsSize - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            result[1] = mid;
+            left = mid + 1;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+
+    return result;
+}
+
+
+#ifndef MAIN_FUNCTION
+#define MAIN_FUNCTION
+
+
+
+
+
+
+#endif
